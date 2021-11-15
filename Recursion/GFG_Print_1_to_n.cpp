f93672db7e7c537c6862358c2d9b9f3e2db3b17e
Deepali Thakur <77734636+deepalii05@users.noreply.github.com>
2021-11-16 00:36:04 +0530

    Create GFG_Print_1_to_n.cpp

---
+++ Recursion/GFG_Print_1_to_n.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+if(N>0){
+void printTillN(int N)
+{if(N>0){
+print TillN(N-1);
+cout<<N;
+}
+}
+};
+int main(){
+int t; 
+cin >> t; 
+while (t--)
+int N;
+cin>>N; 
+Solution ab;
+ob.printTillN(N);
+cout <<endl;
+return 0;
+}
